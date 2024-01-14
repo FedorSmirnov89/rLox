0a470b6eb031450e94d024713742c50214654484
Fedor Smirnov <fedorsmirnov89@gmail.com>
2024-01-14 09:39:01 +0100

    Declaring and asigning vars working

--- src/interpreter/statements/var_declaration.rs
+++ src/interpreter/statements/var_declaration.rs
@@ -1,4 +1,8 @@
-use crate::{domain::grammar::VarDeclaration, interpreter::error::InterpreterError, State, Value};
+use crate::{
+    domain::grammar::VarDeclaration,
+    interpreter::{error::InterpreterError, expressions::InterpretedExpression},
+    State, Value,
+};
 
 use super::InterpretedStatement;
 
@@ -6,12 +10,12 @@ impl InterpretedStatement for VarDeclaration {
     fn interpret_statement(&self, state: &mut State) -> Result<(), InterpreterError> {
         let iden = match self {
             VarDeclaration::Declare(i) => i,
-            VarDeclaration::DeclareAndAssign(_, _) => todo!(),
+            VarDeclaration::DeclareAndAssign(i, _) => i,
         };
 
         let value = match self {
             VarDeclaration::Declare(_) => Value::nil(),
-            VarDeclaration::DeclareAndAssign(_, _) => todo!(),
+            VarDeclaration::DeclareAndAssign(_, e) => e.interpret_expression()?,
         };
 
         state.set_var_value(iden, value);
--- tests/api/interpreter/statements.rs
+++ tests/api/interpreter/statements.rs
@@ -3,7 +3,7 @@ use rlox::ValueType;
 use crate::TestApp;
 
 #[test]
-fn declared_variable_is_part_of_state() {
+fn declared_variable_is_set_to_nil_if_no_assign() {
     // Arrange
     let input = "var a;";
     let mut test_app = TestApp::spawn();
@@ -15,4 +15,19 @@ fn declared_variable_is_part_of_state() {
     let var = test_app.interpreter_state().get_var_value("a");
     assert!(var.is_some(), "declared variable not in state");
     assert_eq!(ValueType::Nil, var.unwrap().v_type);
+}
+
+#[test]
+fn declared_and_assign_var_is_in_state() {
+    // Arrange
+    let input = "var a = 1;";
+    let mut test_app = TestApp::spawn();
+
+    // Act - interpret the input
+    test_app.process_input(input).unwrap();
+
+    // Assert - check that variable is present and its value is nil
+    let var = test_app.interpreter_state().get_var_value("a");
+    assert!(var.is_some(), "declared variable not in state");
+    assert_eq!(ValueType::Number(1.0), var.unwrap().v_type);
 }
