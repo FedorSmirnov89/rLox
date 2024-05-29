54e34e32f2c17fbfe2e8388ace25a2d838f766c3
Fedor Smirnov <fedorsmirnov89@gmail.com>
2024-05-29 17:09:03 +0200

    First draft of block as expression

---
+++ src/interpreter/expressions/block.rs
@@ -0,0 +1,36 @@
+use crate::{
+    domain::grammar::Block,
+    interpreter::{error::InterpreterError, statements::InterpretedStatement},
+    Environment, Value,
+};
+
+use super::InterpretedExpression;
+
+impl InterpretedExpression for Block {
+    fn interpret_expression(
+        &self,
+        env: &mut Environment,
+    ) -> Result<crate::Value, InterpreterError> {
+        env.new_inner_scope();
+        let inner_result = self.interpret_statements_in_inner_scope(env);
+        env.teardown_inner_scope();
+        inner_result
+    }
+}
+
+impl Block {
+    fn interpret_statements_in_inner_scope(
+        &self,
+        env: &mut Environment,
+    ) -> Result<Value, InterpreterError> {
+        // interpret all statetements
+        for statement in &self.statements {
+            statement.interpret_statement(env)?;
+        }
+        if let Some(exp) = self.final_expression {
+            exp.interpret_expression(env)
+        } else {
+            Ok(Value::nil())
+        }
+    }
+}
